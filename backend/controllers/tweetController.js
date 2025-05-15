c8bf32300e6033554a5e95a51417fe8b6100ea86
QASIM-LOKHANDWALA <qasimlokhandwala1@gmail.com>
2025-05-15 17:33:21 +0530

    Updated Tweet Schema

--- backend/controllers/tweetController.js
+++ backend/controllers/tweetController.js
@@ -10,10 +10,11 @@ export const CreateTweet = async (req, res) => {
                 success: false,
             });
         }
-
+        const user = await User.findById(id).select("-password");
         await Tweet.create({
             description,
             userId: id,
+            userDetails: user,
         });
         return res.status(201).json({
             message: "Tweet created successfully.",
--- backend/models/tweetSchema.js
+++ backend/models/tweetSchema.js
@@ -14,6 +14,10 @@ const tweetSchema = mongoose.Schema(
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
         },
+        userDetails: {
+            type: Array,
+            default: [],
+        },
     },
     {
         timestamps: true,
